428f100a0b8e8176d7a417b3cec77c70d207ee99
kavitha <kichenassamy@et.esiea.fr>
2022-04-03 02:35:27 +0200

    Panier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
-import { BarComponent } from './component/bar/bar.component';
-import { BannerComponent } from './component/banner/banner.component';
 import { ProductsComponent } from './component/products/products.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PanierComponent } from './component/panier/panier.component';
@@ -16,8 +14,6 @@ import { PanierComponent } from './component/panier/panier.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BarComponent,
-    BannerComponent,
     ProductsComponent,
     PanierComponent
   ],
